f21a8d595d0cf78fe8d24982be0ec09e1eb66c88
Adilson Júnior <Juniorkadosh12@gmail.com>
2022-02-07 22:17:10 -0300

    Add home content

--- carcara-app/App/components/templates/Home/home-page.tsx
+++ carcara-app/App/components/templates/Home/home-page.tsx
@@ -1,8 +1,18 @@
-import styles from "./HomePage.module.scss";
+import styles from "./home-page.module.scss";
 
 const HomePage = () => (
   <div>
-    <h1 className="styles">CARCARA !</h1>
+    <h2 className={styles.h1}>Estude na Carcara!</h2>
+    <p>
+      A Cod3r se preocupa com o seu futuro e oferece os melhores cursos nas{" "}
+      <br />
+      principais tecnologias com excelência. Os nossos professores e métodos te{" "}
+      <br />
+      darão a base e os fundamentos para você aprender os porquês e trilhar os
+      caminhos do sucesso.
+    </p>
+    <button className={styles.btn}>Nossos cursos</button>
+    <button className={styles.btn2}>Plano de Assinatura</button>
   </div>
 );
 
